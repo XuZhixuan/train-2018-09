8be36a1754492e0d1147e1e83388e6c5238d409a
XuZhixuan <sxxuzhixuan@hotmail.com>
2018-09-11 23:52:39 +0800

    修改配置，增加基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/xuzhixuan" target="_blank">f(x,z)=xzx</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="http://www.miitbeian.gov.cn">陕ICP备17019821号</a></p>
+    </div>
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h5>这里是主页</h5>
+@endsection
